20ebddd1bca7c6b1a6ac425caab31b81098c97c8
Nhật Thái Quốc <thaiquocnhat96@gmail.com>
2016-11-09 23:37:58 +0700

    Add Entity Framework Code First for Data Layer

---
+++ CarManagement.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManagement.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+                DisposeCore();
+            isDisposed = true;
+        }
+
+        //Ovverride this to dispose custom Object
+        protected virtual void DisposeCore() { }
+    }
+}
